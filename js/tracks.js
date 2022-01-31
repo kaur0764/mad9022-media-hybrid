eae8f491e050d32dd7d96672a6891eb5a266918e
JasreetK <90123505+JasreetK@users.noreply.github.com>
2022-01-31 18:44:29 +0530

    Add tracks.js file representing mp3 files

---
+++ js/tracks.js
@@ -0,0 +1,56 @@
+const TRACKS = [
+  {
+    artist: 'ItsWatR',
+    title: 'Let-it-g',
+    src: 'media/let-it-go.mp3',
+    img: 'media/let-it-go.jpg'
+  },
+  {
+    artist: 'madirfan',
+    title: 'Hidden place',
+    src: 'media/hidden-place.mp3',
+    img: 'media/hidden-place.jpg'
+  },
+  {
+    artist: 'SergeQuadrado',
+    title: 'Moment',
+    src: 'media/moment.mp3',
+    img: 'media/moment.jpg'
+  },
+  {
+    artist: 'BeCorbal',
+    title: 'Town',
+    src: 'media/town.mp3',
+    img: 'media/town.jpg'
+  },
+  {
+    artist: 'BeCorbal',
+    title: 'For me',
+    src: 'media/for-me.mp3',
+    img: 'media/for-me.jpg'
+  },
+  {
+    artist: 'ZakharValaha',
+    title: 'Piano moment',
+    src: 'media/piano-moment.mp3',
+    img: 'media/piano-moment.jpg'
+  },
+  {
+    artist: 'ItsWatR',
+    title: 'Cali',
+    src: 'media/cali.mp3',
+    img: 'media/cali.jpg'
+  },
+  {
+    artist: 'LiteSaturation',
+    title: 'Fantasy magic',
+    src: 'media/fantasy-magic.mp3',
+    img: 'media/fantasy-magic.jpg'
+  },
+  {
+    artist: 'lvymusic',
+    title: 'Future bass',
+    src: 'media/future-bass.mp3',
+    img: 'media/future-bass.jpg'
+  }
+]
