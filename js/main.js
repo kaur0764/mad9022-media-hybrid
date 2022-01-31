a5e2671916794cf50d4242a7730bc60aada6ed63
JasreetK <90123505+JasreetK@users.noreply.github.com>
2022-01-31 19:10:17 +0530

    Add source to audio element through js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 import TRACKS from "./tracks.js"; 
+
+const APP = {
+  audio: document.getElementById('player-audio'),
+  currentTrack: 0,
+  init : () => { 
+    PLAYING.trackPlaying()
+  }
+}
+
+/* For anything related to Track playing */
+const PLAYING = {
+  trackPlaying(){
+    APP.audio.src = TRACKS[APP.currentTrack].src
+  }
+}
+
+document.addEventListener('DOMContentLoaded', APP.init);
